37ec5d9c3297e17dd17ba873a219e12caa0a3b1f
Michal Jarnot <m.jarnot@yahoo.com>
2020-05-12 20:02:01 +0200

    feat: meh

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,6 @@ import { preserveShebangs } from 'rollup-plugin-preserve-shebangs'
 import progress from 'rollup-plugin-progress'
 import typescript from 'rollup-plugin-typescript2'
 
-const tsDefaults = { compilerOptions: { declaration: true } }
-const tsOverrides = { compilerOptions: { declaration: false } }
-
 export default ['bin', 'index'].map(name => ({
   input: `./src/${name}.ts`,
   output: {
@@ -19,8 +16,6 @@ export default ['bin', 'index'].map(name => ({
     typescript({
       clean: true,
       tsconfig: 'tsconfig.build.json',
-      tsconfigDefaults: tsDefaults,
-      tsconfigOverride: tsOverrides,
     }),
     commonjs(),
   ],
