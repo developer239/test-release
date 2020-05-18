36e248d77b66971f88c5c03317e0f5d01b8ce90a
michaljarnot <m.jarnot@yahoo.com>
2020-05-18 16:16:51 +0200

    chore: setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: [
     '@linters/eslint-config-node',
     '@linters/eslint-config-typescript',
+    '@linters/eslint-config-jest',
     'prettier',
   ],
+  rules: {
+    'security/detect-object-injection': 0,
+    'guard-for-in': 1,
+  }
 }
