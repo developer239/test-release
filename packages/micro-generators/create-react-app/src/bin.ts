bd5b40c4d9689688151a19f00a8e90a114d09bfa
michaljarnot <m.jarnot@yahoo.com>
2020-10-10 03:33:57 +0200

    chore: ask project name

--- packages/micro-generators/create-react-app/src/bin.ts
+++ packages/micro-generators/create-react-app/src/bin.ts
@@ -1,10 +1,9 @@
-import { execute, getProjectPath, logError } from '@test-release/core'
+import { execute, askProjectName, logError } from '@test-release/core'
 import { askIsRouter } from './services/prompt'
 import { createCreateReactAppSchema } from './index'
 
 const run = async () => {
-  // TODO: replace with `askProjectName`
-  const projectFolder = getProjectPath() ?? '.'
+  const projectFolder = await askProjectName()
   const isRouter = await askIsRouter()
 
   const createReactAppSchema = createCreateReactAppSchema({
